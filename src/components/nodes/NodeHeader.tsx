9933402ba9d12699f8631120b014f706d7282f55
Julio Cezar da Silva <jcezardasilva@outlook.com>
2025-05-17 22:57:21 -0300

    fix: node header

--- src/components/nodes/NodeHeader.tsx
+++ src/components/nodes/NodeHeader.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
 interface NodeHeaderProps {
   name: string;
@@ -17,7 +19,9 @@ const NodeHeader: React.FC<NodeHeaderProps> = ({
   return (
     <div className={`node-header ${className} d-flex justify-content-between bg-transparent border-0 text-white-50 w-100`}>
       <span>{name}</span>
-      <button onClick={onEdit} style={{ background: 'none', border: 'none', marginRight: -15, color: 'white', fontSize: 18, cursor: 'pointer' }}>⚙️</button>
+      <button onClick={onEdit} style={{ background: 'none', border: 'none',  color: 'white', fontSize: 18, cursor: 'pointer' }}>
+        <FontAwesomeIcon icon={faEdit} />
+      </button>
     </div>
   );
 };
